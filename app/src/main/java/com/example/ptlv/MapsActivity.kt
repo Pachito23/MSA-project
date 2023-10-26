f39ecdbdfa8350736f36380ef066bc147cb915e9
Pasquale Vivaldo <98670552+Pachito23@users.noreply.github.com>
2023-10-26 18:37:35 +0300

    Main view & map view with back button

--- app/src/main/java/com/example/ptlv/MapsActivity.kt
+++ app/src/main/java/com/example/ptlv/MapsActivity.kt
@@ -1,15 +1,17 @@
 package com.example.ptlv
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.ptlv.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.Marker
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -19,6 +21,69 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     var marker: Marker? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+
+        MainView()
+    }
+
+    fun MapView() {
+        setContentView(R.layout.activity_maps)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        val BackButton = findViewById<ImageView>(R.id.BackButton)
+
+        BackButton.setOnClickListener {
+            // Code to be executed when the button is clicked
+            // For example, you can display a toast message
+            Toast.makeText(this, "Button Clicked!", Toast.LENGTH_SHORT).show()
+            MainView()
+        }
+    }
+    fun MainView(){
+        setContentView(R.layout.activity_main)
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.spinner_options, android.R.layout.simple_spinner_item
+        )
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedItem = parent?.getItemAtPosition(position).toString()
+                // Do something with the selected item
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Do something when nothing is selected
+            }
+        }
+
+        val myButton = findViewById<Button>(R.id.Map)
+
+        myButton.setOnClickListener {
+            // Code to be executed when the button is clicked
+            // For example, you can display a toast message
+            Toast.makeText(this, "Button Clicked!", Toast.LENGTH_SHORT).show()
+            MapView()
+        }
+    }
+
+    /*override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
@@ -30,7 +95,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
 
         move_marker_to(mMap,12.0,12.0)
-    }
+    }*/
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
@@ -49,4 +114,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             Thread.sleep(1000)
         }
     }
+
+    //TO DO: use fragments to switch view
 }
