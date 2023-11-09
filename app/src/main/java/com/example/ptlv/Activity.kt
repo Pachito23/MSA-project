6c3c1a420a73d5f9d7fb55c1f60bcf35736f89de
Pasquale Vivaldo <98670552+Pachito23@users.noreply.github.com>
2023-11-09 18:35:48 +0200

    Adding current position on app 1

--- app/src/main/java/com/example/ptlv/Activity.kt
+++ app/src/main/java/com/example/ptlv/Activity.kt
@@ -1,12 +1,22 @@
 package com.example.ptlv
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.media.audiofx.Equalizer.Settings
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.example.ptlv.databinding.FragmentMapBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -14,12 +24,18 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.Task
 
 class Activity : AppCompatActivity(), OnMapReadyCallback {
+
+    var FINE_PERMISSION_CODE = 1
+    lateinit var currentLocation: Location
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     private lateinit var mMap: GoogleMap
     lateinit var binding: FragmentMapBinding
     var marker: Marker? = null
+
     companion object{
         var map = MapFragment()
         var main = MainFragment()
@@ -28,9 +44,74 @@ class Activity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         replaceFragment(main)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        getLastLocation()
     }
 
+    private fun getLastLocation()
+    {
+
+        if(checkPermissions())
+        {
+            Toast.makeText(this,"Permissions OK",Toast.LENGTH_SHORT).show()
+            if (locationEnabled())
+            {
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION),FINE_PERMISSION_CODE)
+                    return
+                }
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener(this){ task ->
+                    val location:Location?=task.result
+                    if(location==null)
+                    {
+                        Toast.makeText(this,"Error retrieving location",Toast.LENGTH_SHORT).show()
+                    }
+                    else
+                    {
+                        Toast.makeText(this,"Latitude: " + location.latitude + "\nLongitude: " + location.longitude,Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            else
+            {
+               Toast.makeText(this,"Turn on location",Toast.LENGTH_SHORT).show()
+                //val intent = Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                //startActivity(intent)
+            }
+
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),FINE_PERMISSION_CODE)
+        }
+    }
+
+    private fun locationEnabled():Boolean
+    {
+        val locationManager:LocationManager=getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)||locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun checkPermissions(): Boolean
+    {
+        return ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED;
+    }
     fun replaceFragment(fragment: Fragment) {
         val supportFragmentManager: FragmentManager = supportFragmentManager
         val fragmentTransaction = supportFragmentManager.beginTransaction()
