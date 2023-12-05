c7e67b26f8d89cadb14bda519f0e5ef917d6d462
Pasquale Vivaldo <98670552+Pachito23@users.noreply.github.com>
2023-12-05 13:23:10 +0200

    Added Markers based on database info automatically

--- app/src/main/java/com/example/ptlv/MapFragment.kt
+++ app/src/main/java/com/example/ptlv/MapFragment.kt
@@ -1,6 +1,5 @@
 package com.example.ptlv
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,9 +13,13 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.Tasks
+import com.google.firebase.database.*
 
 class MapFragment : Fragment(), OnMapReadyCallback {
 
+    var firebaseDatabase: FirebaseDatabase? = null
+    var databaseReference: DatabaseReference? = null
     private lateinit var mMap: GoogleMap
     var marker: Marker? = null
 
@@ -72,6 +75,39 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         }
     }
 
+    data class Stop(val name:String, val lat:String, val long:String)
+    var stop_list:MutableList<Stop> = mutableListOf()
+
+    fun get_stops(type:String , line: String) {
+
+        //firebase realtime database references
+        firebaseDatabase = FirebaseDatabase.getInstance("https://ptlv-402713-default-rtdb.europe-west1.firebasedatabase.app")
+        databaseReference = firebaseDatabase!!.getReference(type)
+
+        //we add an event listener to verify when the data is changed
+        databaseReference!!.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val line = snapshot!!.child(line)
+                val nr_stops = line.child("Stops").value.toString().toInt()
+                stop_list.clear()
+                for(stop in 1..nr_stops)
+                {
+                    val stop_name:String = line.child("Stop$stop").child("name").value.toString()
+                    val stop_lat:String = line.child("Stop$stop").child("lat").value.toString()
+                    val stop_long:String = line.child("Stop$stop").child("long").value.toString()
+                    stop_list.add(Stop(stop_name, stop_lat, stop_long))
+                }
+                println("\n\n" + stop_list + "\n\n")
+                updateMarkers()
+            }
+
+            //error getting the data
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(requireContext(), "Fail to get data.", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
     override fun onMapReady(my_map: GoogleMap) {
         mMap = my_map
 
@@ -82,7 +118,27 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 //                .position(sydney)
 //                .title("Marker in Sydney")
 //        )
+
+        get_stops("Bus","Line-33")
         marker = mMap.addMarker(MarkerOptions().position(location).title("Marker in Timisoara"))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
+    }
+
+    fun updateMarkers()
+    {
+        println(stop_list.size)
+        for(stop in 0 until stop_list.size)
+        {
+            val stop_name:String = stop_list[stop].name
+            val stop_lat:Double = stop_list[stop].lat.toDouble()
+            val stop_long:Double = stop_list[stop].long.toDouble()
+
+            val pos = LatLng(stop_lat, stop_long)
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(pos)
+                    .title(stop_name)
+            )
+        }
     }
 }
